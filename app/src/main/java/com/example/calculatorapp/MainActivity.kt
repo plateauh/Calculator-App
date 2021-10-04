02f04f48747c55b6cab9df5e6571d9cd86108ea9
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-04 10:35:53 +0300

    I dont like the text direction

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -9,28 +9,6 @@ import android.widget.TextView
 private lateinit var resultTextView: TextView
 private lateinit var buttonsList: ArrayList<Button>
 
-//private lateinit var btn0: Button
-//private lateinit var btn1: Button
-//private lateinit var btn2: Button
-//private lateinit var btn3: Button
-//private lateinit var btn4: Button
-//private lateinit var btn5: Button
-//private lateinit var btn6: Button
-//private lateinit var btn7: Button
-//private lateinit var btn8: Button
-//private lateinit var btn9: Button
-
-private lateinit var deleteBtn: Button
-private lateinit var clearBtn: Button
-
-private lateinit var signBtn: Button
-private lateinit var decimalBtn: Button
-private lateinit var equalBtn: Button
-
-private lateinit var addBtn: Button
-private lateinit var subtractBtn: Button
-private lateinit var multiplyBtn: Button
-private lateinit var divideBtn: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         resultTextView = findViewById(R.id.result_tv)
         Log.d("ID", resultTextView.id.toString())
-//        initiateButtons()
+        initiateButtons()
 
 
     }
@@ -68,43 +46,70 @@ class MainActivity : AppCompatActivity() {
         )
 
         for (button in buttonsList)
-            button.setOnClickListener { numberEntries(button.id) }
+            button.setOnClickListener { entries(button.id) }
     }
 
 
-    private fun numberEntries (viewID: Int){
+    private fun entries (viewID: Int){
 
         val currentOperation = resultTextView.text.toString()
         when (viewID) {
             R.id.number_0 -> {
-                resultTextView.text = "$currentOperation 0"
+                resultTextView.text = "${currentOperation}0"
             }
             R.id.number_1 -> {
-                resultTextView.text = "$currentOperation 1"
+                resultTextView.text = "${currentOperation}1"
             }
             R.id.number_2 -> {
-                resultTextView.text = "$currentOperation 2"
+                resultTextView.text = "${currentOperation}2"
             }
             R.id.number_3 -> {
-                resultTextView.text = "$currentOperation 3"
+                resultTextView.text = "${currentOperation}3"
             }
             R.id.number_4 -> {
-                resultTextView.text = "$currentOperation 4"
+                resultTextView.text = "${currentOperation}4"
             }
             R.id.number_5 -> {
-                resultTextView.text = "$currentOperation 5"
+                resultTextView.text = "${currentOperation}5"
             }
             R.id.number_6 -> {
-                resultTextView.text = "$currentOperation 6"
+                resultTextView.text = "${currentOperation}6"
             }
             R.id.number_7 -> {
-                resultTextView.text = "$currentOperation 7"
+                resultTextView.text = "${currentOperation}7"
             }
             R.id.number_8 -> {
-                resultTextView.text = "$currentOperation 8"
+                resultTextView.text = "${currentOperation}8"
             }
             R.id.number_9 -> {
-                resultTextView.text = "$currentOperation 9"
+                resultTextView.text = "${currentOperation}9"
+            }
+            R.id.delete_btn -> {
+                resultTextView.text = currentOperation.slice(0..currentOperation.length-2) // correct?
+            }
+            R.id.clear_btn -> {
+                resultTextView.text = ""
+            }
+            R.id.sign_btn -> {
+                resultTextView.text = "$currentOperation -"
+            }
+            R.id.decimal_btn -> {
+                resultTextView.text = "${currentOperation}."
+            }
+            R.id.equal_btn -> {
+                // compute
+            }
+            R.id.addition_btn -> {
+                resultTextView.text = "$currentOperation + "
+            }
+            R.id.subtraction_btn -> {
+                resultTextView.text = "$currentOperation - "
+            }
+            R.id.multiply_btn -> {
+                resultTextView.text = "$currentOperation * "
+            }
+            R.id.divide_btn -> {
+                resultTextView.text = "$currentOperation / "
             }
         }
     }
